6527588a8b23563a48a687aa9304d13223ebf7be
adamhoof <adam.homfna1@gmail.com>
2022-08-11 22:02:29 +0200

    create rtos task to handle wifi connection checking

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,30 @@
 
 WifiController wifiController{};
 
+void maintainWifiConnectionRTOS(void * parameters) {
+    for(;;) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        wifiController.maintainConnection();
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
     wifiController.setSSID(wifiSSID).setPassword(wifiPassword);
-    wifiController.setHostname("Shit");
+    wifiController.setHostname(hostname);
+
+    xTaskCreatePinnedToCore(
+            maintainWifiConnectionRTOS,
+            "keepWifiAlive",
+            5000,
+            nullptr,
+            1,
+            nullptr,
+            CONFIG_ARDUINO_RUNNING_CORE
+            );
 }
 
 void loop()
 {
-    wifiController.maintainConnection();
 }
