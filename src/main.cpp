58b3f4dc904634d420224edc4f3b9bff0fbe85ce
adamhoof <adam.homfna1@gmail.com>
2022-08-11 21:47:49 +0200

    basic wifi connection estabilish

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <Arduino.h>
 #include <WifiController.h>
+#include "credentials.h"
 
 WifiController wifiController{};
 
 void setup()
 {
     Serial.begin(115200);
-    wifiController.setSSID("Gei").setPassword("1234567890");
+    wifiController.setSSID(wifiSSID).setPassword(wifiPassword);
     wifiController.setHostname("Shit");
 }
 
 void loop()
 {
-
+    wifiController.maintainConnection();
 }
