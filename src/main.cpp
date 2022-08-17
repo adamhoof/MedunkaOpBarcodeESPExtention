d3425508a6d145deb47eac55e5e975e0dfe95aa6
adamhoof <adam.homfna1@gmail.com>
2022-08-17 13:36:32 +0200

    add demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #include <WifiController.h>
 #include "credentials.h"
+#include <Adafruit_ILI9341.h>
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Fonts/FreeSansBold9pt7b.h>
 
 WifiController wifiController {};
 SoftwareSerial softwareSerial {};
+
+const uint8_t displayCSPin = 32;
+const uint8_t displayDCPin = 26;
+const uint8_t displayRSTPin = 25;
+
+Adafruit_ILI9341 display = Adafruit_ILI9341(displayCSPin, displayDCPin, displayRSTPin);
 
 void maintainWifiConnectionRTOS(void* parameters)
 {
@@ -16,8 +25,24 @@ void maintainWifiConnectionRTOS(void* parameters)
 
 void setup()
 {
+    display.begin();
+    display.setRotation(3);
+    display.fillScreen(ILI9341_BLACK);
+    display.setCursor(0, 0);
+    display.setTextColor(ILI9341_WHITE);
+    display.setTextSize(3);
+    display.println("Nazev: Prirodni mydlo 50g");
+    display.println();
+    display.setTextSize(4);
+    display.setTextColor(ILI9341_MAGENTA);
+    display.println("Cena: 50 kc");
+    display.println();
+    display.setTextSize(2);
+    display.setTextColor(ILI9341_WHITE);
+    display.println("Stock: 2");
+    display.println();
     Serial.begin(115200);
-    softwareSerial.begin(9600, SWSERIAL_8N1, 19, 23);
+    /*softwareSerial.begin(9600, SWSERIAL_8N1, 13, 15);
     wifiController.setSSID(wifiSSID).setPassword(wifiPassword);
     wifiController.setHostname(hostname);
 
@@ -29,7 +54,7 @@ void setup()
             1,
             nullptr,
             CONFIG_ARDUINO_RUNNING_CORE
-    );
+    );*/
 }
 
 void loop()
