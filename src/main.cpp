5798730931b8feca5059bcdfbce00ab04dbc9995
adamhoof <adam.hofman1@gmail.com>
2022-09-05 09:40:37 +0200

    working requests

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 #include <Adafruit_ILI9341.h>
 #include <SoftwareSerial.h>
 #include <SPI.h>
-#include <Fonts/FreeMonoBoi.h>
+#include <Fonts/arial.h>
+#include "credentials.h"
+#include <HTTPClient.h>
+#include <utility>
+#include <ArduinoJson.h>
 
 WifiController wifiController {};
 SoftwareSerial softwareSerial {};
@@ -22,22 +26,43 @@ void maintainWifiConnectionRTOS(void* parameters)
     }
 }
 
+struct Response
+{
+    String payload {};
+    int code {};
+
+    Response(int code, String payload)
+    {
+        this->code = code;
+        this->payload = std::move(payload);
+    }
+};
+
+Response getProductData(const char* requestUrl)
+{
+    WiFiClient client;
+    HTTPClient http;
+
+    http.begin(client, requestUrl);
+
+    Response response = Response(http.GET(), http.getString());
+
+    http.end();
+
+    return response;
+}
+
 void setup()
 {
     display.begin();
-    display.setFont(&FreeMono9pt8b);
+    display.setFont(&ariblk9pt8b);
     display.setRotation(3);
     display.fillScreen(ILI9341_BLACK);
-    display.setCursor(0, 0);
-    display.setTextColor(ILI9341_WHITE);
-    display.setTextSize(1);
-    display.println();
-    display.println("ěščřžýáíé");
 
     Serial.begin(115200);
     softwareSerial.begin(9600, SWSERIAL_8N1, 13, 15);
 
-    /*wifiController.setHostname("MedunkaOPBarcodeUpdate").setSSID("Gei").setPassword("1234567890");
+    wifiController.setHostname(hostname).setSSID(wiFiSSID).setPassword(wiFiPassword);
     xTaskCreatePinnedToCore(
             maintainWifiConnectionRTOS,
             "keepWifiAlive",
@@ -46,17 +71,50 @@ void setup()
             1,
             nullptr,
             CONFIG_ARDUINO_RUNNING_CORE
-    );*/
+    );
 }
 
 void loop()
 {
     if (softwareSerial.available() > 0) {
-        uint8_t dataBuffer[30];
-        uint8_t size = softwareSerial.readBytesUntil(13, dataBuffer, 30);
-        char chars[size+1];
-        memcpy(chars, dataBuffer, size);
+        uint8_t barcodeBuffer[22];
+        uint8_t size = softwareSerial.readBytesUntil(13, barcodeBuffer, 30);
+        char chars[size + 1];
+        memcpy(chars, barcodeBuffer, size);
         chars[size] = '\0';
-        Serial.println(chars);
+
+        /*const char* requestArguments = "?barcode=8594052880977&include_diacritics=false";
+        char* request_url = new char[strlen(serverName) + strlen(requestArguments) + 1];
+        strcpy(request_url, serverName);
+        strcat(request_url, requestArguments);*/
+
+        Response response = getProductData(request_url);
+        Serial.println(response.payload);
+
+        StaticJsonDocument<250> productData;
+
+        DeserializationError error = deserializeJson(productData, response.payload.c_str());
+
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.f_str());
+            return;
+        }
+
+        const char* name = productData["Name"];
+
+        Serial.println(name);
+
+        /*display.setTextColor(ILI9341_WHITE);
+        display.fillScreen(ILI9341_BLACK);
+        display.setTextSize(1);
+        display.setCursor(0, 20);
+        display.println("Bylinna mast pro psy 50g");
+        display.println();
+        display.println();
+
+        display.setTextColor(ILI9341_RED);
+        display.setTextSize(2);
+        display.println("Cena: 50 kc");*/
     }
 }
